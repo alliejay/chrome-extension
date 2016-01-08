2fe450d2ad070552cfb800d94c0611fc12e5955e
Alison Arsenault <AlisonHorta@Alisons-MacBook-Pro.local>
2016-01-07 21:23:49 -0500

    Made location adjustable using zip codes

--- popup.js
+++ popup.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
   page.init();
 });
 
+var zip = 29464;
+
 var page = {
 
   init: function () {
@@ -14,21 +16,26 @@ var page = {
   },
 
   initEvents: function () {
+    $('.newLocation').on('submit', function(event) {
+      event.preventDefault();
+      zip = $('#newZip').val();
+      page.getWeather();
+    });
   },
 
   getWeather: function(){
   $.ajax({
-    url: 'http://api.wunderground.com/api/424d5cc3ad79524b/conditions/q/SC/Charleston.json',
+    url: 'http://api.wunderground.com/api/424d5cc3ad79524b/conditions/q/' + zip + '.json',
     method: 'GET',
     success: function(data) {
-        $('.weather').append (
+        $('.weather').html (
         "<h1>Location: " + data.current_observation.display_location.full +"</h1>" +
         "<img src='" + data.current_observation.icon_url + "'/><br>" +
         "<h2>Currently: " + data.current_observation.weather + " <br>" +
         data.current_observation.temp_f + "&deg; F with " +
         data.current_observation.relative_humidity + " humidty<br>" +
         "Feels like: " + data.current_observation.feelslike_f + "&deg;</h2><br>");
-        $('footer').append("<a href='http://www.wunderground.com/US/SC/Charleston.html'>Full Weather Report</a> <br><br>" +
+        $('footer').html("<a href='http://www.wunderground.com/US/SC/Charleston.html'>Full Weather Report</a> <br><br>" +
         "<h3>Brought to you by <a href='http://www.wunderground.com'> Weather Underground <img src='" + data.current_observation.image.url +"' class='logo' /></a></h3>");
         $('footer').on('click', 'a', function(){
              chrome.tabs.create({
@@ -36,6 +43,6 @@ var page = {
                });
            });
        }
-    })
+    });
   }
 };
